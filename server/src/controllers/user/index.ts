3ea94df4bdd51b9ce5a0b490c95720dddcd131e5
Maximilian Svensson <Max.svensson@outlook.com>
2022-10-21 19:51:02 +0200

    Added number

--- server/src/controllers/user/index.ts
+++ server/src/controllers/user/index.ts
@@ -2,29 +2,29 @@ import { RequestHandler } from "express";
 import prisma from "../../database/prisma";
 
 import {
-    checkPasswordMatch,
-    CreateSessionToken,
-    CreateUser,
-    FindUserByEmail,
+  checkPasswordMatch,
+  CreateSessionToken,
+  CreateUser,
+  FindUserByEmail,
 } from "../../services/user/index";
 
 const updateLimit: RequestHandler = async (req, res) => {
-    try {
-        const { limit } = req.body as { limit: number };
+  try {
+    const { limit } = req.body as { limit: number };
 
-        await prisma.user.update({
-            where: { id: req.user.id },
-            data: { limit: limit },
-        });
+    await prisma.user.update({
+      where: { id: req.user.id },
+      data: { limit: limit },
+    });
 
-        res.status(201).send({
-            message: "Limit set",
-        });
-    } catch (error) {
-        res.status(400).send({
-            message: "Limits was not successfully created",
-        });
-    }
+    res.status(201).send({
+      message: "Limit set",
+    });
+  } catch (error) {
+    res.status(400).send({
+      message: "Limits was not successfully created",
+    });
+  }
 };
 
 // const updateNumbers: RequestHandler = async (req, res) => {
@@ -47,83 +47,82 @@ const updateLimit: RequestHandler = async (req, res) => {
 // };
 
 const registerUser: RequestHandler = async (req, res) => {
-    console.log("frw");
-    try {
-        const { email, name, password } = req.body;
+  try {
+    const { email, name, password, number } = req.body;
 
-        if (!email || !password || !name) {
-            throw new Error("Not all data provided");
-        }
-
-        const user = await CreateUser({ email, name, password });
-
-        res.status(201).send(user);
-    } catch (error) {
-        console.log(error);
-        res.status(400).send({
-            message: error.message,
-            error: true,
-        });
+    if (!email || !password || !name) {
+      throw new Error("Not all data provided");
     }
+
+    const user = await CreateUser({ email, name, password, number });
+
+    res.status(201).send(user);
+  } catch (error) {
+    console.log(error);
+    res.status(400).send({
+      message: error.message,
+      error: true,
+    });
+  }
 };
 
 const GetUser: RequestHandler = async (req, res) => {
-    try {
-        if (!req.user) {
-            throw new Error("No user was found");
-        }
-
-        res.status(200).send({
-            user: req.user,
-        });
-    } catch (error) {
-        res.status(404).send({
-            message: "User was not found",
-        });
+  try {
+    if (!req.user) {
+      throw new Error("No user was found");
     }
+
+    res.status(200).send({
+      user: req.user,
+    });
+  } catch (error) {
+    res.status(404).send({
+      message: "User was not found",
+    });
+  }
 };
 
 const LoginUser: RequestHandler = async (req, res) => {
-    try {
-        const { email, password } = req.body;
-        if (!email || !password) {
-            throw new Error("All fields must be provided");
-        }
-
-        const user = await FindUserByEmail(email);
-
-        if (!user) {
-            throw new Error("Email or password is not correct");
-        }
-
-        if (!(await checkPasswordMatch(password, user.password))) {
-            throw new Error("Email or password is not correct");
-        }
-
-        const token = await CreateSessionToken(user.id);
-
-        res.cookie("token", token, {
-            httpOnly: true,
-        });
-
-        res.status(200).send({
-            message: "User is logged in",
-        });
-    } catch (error) {
-        res.status(400).send({
-            error: error.message,
-        });
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      throw new Error("All fields must be provided");
     }
+
+    const user = await FindUserByEmail(email);
+
+    if (!user) {
+      throw new Error("Email or password is not correct");
+    }
+
+    if (!(await checkPasswordMatch(password, user.password))) {
+      throw new Error("Email or password is not correct");
+    }
+
+    const token = await CreateSessionToken(user.id);
+
+    res.cookie("token", token, {
+      httpOnly: true,
+    });
+
+    res.status(200).send({
+      message: "User is logged in",
+    });
+  } catch (error) {
+    res.status(400).send({
+      error: error.message,
+    });
+  }
 };
 
 const logout: RequestHandler = (_, res) => {
-    try {
-        res.clearCookie("token").send();
-    } catch (error) {
-        res.status(400).send({
-            message: error.message,
-        });
-    }
+  try {
+    res.clearCookie("token").send();
+  } catch (error) {
+    res.status(400).send({
+      message: error.message,
+    });
+  }
 };
 
 export { LoginUser, GetUser, logout, registerUser, updateLimit };
--- server/src/services/user/index.ts
+++ server/src/services/user/index.ts
@@ -1,10 +1,10 @@
-import { Prisma } from '@prisma/client';
-import bcrypt from 'bcryptjs';
-import * as jose from 'jose';
-import { User } from '.prisma/client';
-import prisma from '../../database/prisma';
+import { Prisma } from "@prisma/client";
+import bcrypt from "bcryptjs";
+import * as jose from "jose";
+import { User } from ".prisma/client";
+import prisma from "../../database/prisma";
 
-const jwtKey = '#$#034283!4$£@123mDfe_&_@$_&??+';
+const jwtKey = "#$#034283!4$£@123mDfe_&_@$_&??+";
 const key = new TextEncoder().encode(jwtKey);
 
 const FindUserByID = async (id: string) => {
@@ -12,14 +12,16 @@ const FindUserByID = async (id: string) => {
 };
 
 const FindUserByEmail = async (email: string) => {
-  return await prisma.user.findUnique({ where: { email: email.toLowerCase() } });
+  return await prisma.user.findUnique({
+    where: { email: email.toLowerCase() },
+  });
 };
 
 const CreateUser = async (user: Prisma.UserCreateInput): Promise<User> => {
   const userExist = await FindUserByEmail(user.email);
 
   if (userExist) {
-    throw new Error('User already exists');
+    throw new Error("User already exists");
   }
 
   const password = await bcrypt.hash(user.password, 10);
@@ -29,25 +31,28 @@ const CreateUser = async (user: Prisma.UserCreateInput): Promise<User> => {
       email: user.email.toLowerCase(),
       name: user.email,
       password,
+      number: user.number,
     },
   });
 
-  createdUser.password = '';
+  createdUser.password = "";
 
   return createdUser;
 };
 
 const CreateSessionToken = async (userId: string): Promise<string> => {
   const token = await new jose.SignJWT({ aud: userId })
-    .setProtectedHeader({ alg: 'HS256' })
+    .setProtectedHeader({ alg: "HS256" })
     .setIssuedAt()
-    .setExpirationTime('24h')
+    .setExpirationTime("24h")
     .sign(key);
 
   return token;
 };
 
-const VerifySessionToken = async (token: string): Promise<jose.JWTVerifyResult> => {
+const VerifySessionToken = async (
+  token: string
+): Promise<jose.JWTVerifyResult> => {
   return await jose.jwtVerify(token, key);
 };
 
@@ -62,12 +67,15 @@ const GetUserFromToken = async (token: string) => {
   const user = await FindUserByID(userId);
 
   if (!user) {
-    throw new Error('User was not found');
+    throw new Error("User was not found");
   }
   return user;
 };
 
-const checkPasswordMatch = async (password: string, hashPassword: string): Promise<boolean> => {
+const checkPasswordMatch = async (
+  password: string,
+  hashPassword: string
+): Promise<boolean> => {
   return await bcrypt.compare(password, hashPassword);
 };
 
